3febe7f281895e7c66be8966df3dbd68617802b0
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-09 12:42:37 +0530

    Update CustomException.java

--- src/CustomException.java
+++ src/CustomException.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
 
-// TODO: Create class InvalidAgeException extends Exception
-// Create a constructor that accepts a message string and passes it to super(message)
+// Custom Exception Class
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
 
 public class CustomException {
-    
-    // TODO: Create a static method validate(int age) that throws InvalidAgeException
-    // Check if age < 18, throw new InvalidAgeException("Not eligible to vote")
-    // Else print "Eligible to vote"
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    
-        // TODO: Read age
-        
+        int age = sc.nextInt();
+
+        try {
+            if (age < 18) {
+                throw new InvalidAgeException("Not eligible to vote");
+            } else {
+                System.out.println("Eligible to vote");
+            }
+        } 
+        catch (InvalidAgeException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        sc.close();
+    }
 }
